ba9d27e0b0cab746371be6261ba0100a09eeebaf
Toshi <careyeg@gmail.com>
2019-05-14 16:39:19 -0700

    changes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var containerElement = document.getElementById('container');
 var mallArray = [];
 var votesRemaining = 25;
 var randomIndexArray = [];
+var chartDrawn = false;
 
 // Constructor
 function MallPicture(name){
@@ -41,11 +42,7 @@ new MallPicture('wine-glass');
 
 
 
-
-
-
-
-// Show a random goat
+// Show a random product
 function showARandomMall(imageElement){
   // generate a random number
   var randomIndex = Math.floor(Math.random() * mallArray.length);
@@ -83,12 +80,12 @@ function renderResults(){
 }
 
 // Event handler
-function handleGoatClick(event){
+function handleMallClick(event){
   votesRemaining--;
 
   if(votesRemaining === 0){
     renderResults();
-    containerElement.removeEventListener('click', handleGoatClick);
+    containerElement.removeEventListener('click', handleMallClick);
   }
 
   var mallName = event.target.alt;
@@ -100,13 +97,101 @@ function handleGoatClick(event){
     }
   }
 
-  // when I reach 25 votes, I need to run a function that renders the goat's name and the number of votes
+  // when I reach 25 votes, I need to run a function that renders the product's name and the number of votes
   showARandomMall(mallpic);
   showARandomMall(mallpicTwo);
 }
 
 // Stuff that runs on page load
-containerElement.addEventListener('click', handleGoatClick);
-// Show the first goat
+containerElement.addEventListener('click', handleMallClick);
+// Show the first product
 showARandomMall(mallpic);
 showARandomMall(mallpicTwo);
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++
+// CHART STUFF
+// Charts rendered using Chart JS v.2.8.0
+// http://www.chartjs.org/
+// ++++++++++++++++++++++++++++++++++++++++++++
+
+// Arrays to hold data for the chart
+var votes = [];
+var name = [];
+
+
+var data = {
+  labels: name, // name array we declared earlier
+  datasets: [{
+    data: votes, // votes array we declared earlier
+    backgroundColor: [
+      'bisque',
+      'darkgray',
+      'burlywood',
+      'lightblue',
+      'navy'
+    ],
+    hoverBackgroundColor: [
+      'purple',
+      'purple',
+      'purple',
+      'purple',
+      'purple'
+    ]
+  }]
+};
+
+function drawChart() {
+  var ctx = document.getElementById('name-chart').getContext('2d');
+  nameChart = new Chart(ctx, {
+    type: 'polarArea',
+    data: data,
+    options: {
+      responsive: false,
+      animation: {
+        duration: 2000,
+        easing: 'easeOutBounce',
+      },
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          max: 10,
+          min: 0,
+          stepSize: 1.0,
+        },
+      }],
+    },
+  });
+  chartDrawn = true;
+}
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++
+// EVENT LISTENERS FOR CHART
+// ++++++++++++++++++++++++++++++++++++++++++++
+
+document.getElementById('name-chart').addEventListener('click', function() {
+  drawChart();
+  console.log('chart was drawn');
+});
+
+document.getElementById('list-button').addEventListener('click', function() {
+  showAsList();
+});
+
+// document.getElementById('list-button').addEventListener('click', showSongsAsList);
+
+document.getElementById('name-list').addEventListener('click', function() {
+  document.getElementById('name-list').hidden = true;
+});
+
+document.getElementById('voting').addEventListener('click', function(event) {
+  if (event.target.id !== 'voting') {
+    tallyVote(event.target.id);
+  };
+
+  if (chartDrawn) {
+    nameChart.update();
+  }
+});
