e5c62ffb72b2a19601816c98c83f568b08acd984
Toshi <careyeg@gmail.com>
2019-05-14 17:19:39 -0700

    minor changes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 // Global variables
 var mallpic = document.getElementById('mallpic');
 var mallpicTwo = document.getElementById('mallpic-two');
+var mallpicThree = document.getElementById('mallpic-three');
 var containerElement = document.getElementById('container');
 var mallArray = [];
 var votesRemaining = 25;
@@ -100,6 +101,8 @@ function handleMallClick(event){
   // when I reach 25 votes, I need to run a function that renders the product's name and the number of votes
   showARandomMall(mallpic);
   showARandomMall(mallpicTwo);
+  showARandomMall(mallpicThree);
+
 }
 
 // Stuff that runs on page load
@@ -107,6 +110,9 @@ containerElement.addEventListener('click', handleMallClick);
 // Show the first product
 showARandomMall(mallpic);
 showARandomMall(mallpicTwo);
+showARandomMall(mallpicThree);
+
+
 
 
 // ++++++++++++++++++++++++++++++++++++++++++++
@@ -116,82 +122,3 @@ showARandomMall(mallpicTwo);
 // ++++++++++++++++++++++++++++++++++++++++++++
 
 // Arrays to hold data for the chart
-var votes = [];
-var name = [];
-
-
-var data = {
-  labels: name, // name array we declared earlier
-  datasets: [{
-    data: votes, // votes array we declared earlier
-    backgroundColor: [
-      'bisque',
-      'darkgray',
-      'burlywood',
-      'lightblue',
-      'navy'
-    ],
-    hoverBackgroundColor: [
-      'purple',
-      'purple',
-      'purple',
-      'purple',
-      'purple'
-    ]
-  }]
-};
-
-function drawChart() {
-  var ctx = document.getElementById('name-chart').getContext('2d');
-  nameChart = new Chart(ctx, {
-    type: 'polarArea',
-    data: data,
-    options: {
-      responsive: false,
-      animation: {
-        duration: 2000,
-        easing: 'easeOutBounce',
-      },
-    },
-    scales: {
-      yAxes: [{
-        ticks: {
-          max: 10,
-          min: 0,
-          stepSize: 1.0,
-        },
-      }],
-    },
-  });
-  chartDrawn = true;
-}
-
-
-// ++++++++++++++++++++++++++++++++++++++++++++
-// EVENT LISTENERS FOR CHART
-// ++++++++++++++++++++++++++++++++++++++++++++
-
-document.getElementById('name-chart').addEventListener('click', function() {
-  drawChart();
-  console.log('chart was drawn');
-});
-
-document.getElementById('list-button').addEventListener('click', function() {
-  showAsList();
-});
-
-// document.getElementById('list-button').addEventListener('click', showSongsAsList);
-
-document.getElementById('name-list').addEventListener('click', function() {
-  document.getElementById('name-list').hidden = true;
-});
-
-document.getElementById('voting').addEventListener('click', function(event) {
-  if (event.target.id !== 'voting') {
-    tallyVote(event.target.id);
-  };
-
-  if (chartDrawn) {
-    nameChart.update();
-  }
-});
